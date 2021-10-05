56f0cc781155ce6a5e8c8c9d3cb3f9e2f2e3ab68
Frank Kuo <frankkuo@protonmail.com>
2021-10-05 19:20:55 +0800

    tweak p2

--- hw1/p2.cpp
+++ hw1/p2.cpp
@@ -9,59 +9,50 @@
 #include <vector>
 #include <algorithm>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define lli long long int
-
 
 // x_i = b*p_i+c
 // y_i = a*p_i
 // z_i = z_i
 const int MAX_Z = 1e9;
 const int MIN_Z = 0;
-
+const int MAX_N = 2*(1e6)+10;
 
 using namespace std;
 
-// bool cmpVec(vector<lli> &v_a, vector<lli> &v_b) {
-// 	return (v_a[2] < v_b[2]);
-// }
-
 void insertBIT(vector<int> &BIT, int x) {
-	for (int i=x; i<BIT.size(); i+=i&(-i)) {
-		BIT[i]++;
+	for (; x<BIT.size(); x+=x&(-x)) {
+		BIT[x]++;
 	}
 }
 
-int queryBIT(vector<int> &BIT, int target) {
+int queryBIT(vector<int> &BIT, int t) {
 	int ans=0;
-	for (int i=target; i>0; i-=i&(-i)) {
-		ans += BIT[i];
+	for (; t>0; t-=t&(-t)) {
+		ans += BIT[t];
 	}
 	return ans;
 }
 
-// int BSIndex(vector<lli> v, lli target) {
-// 	int l=0, r=v.size()-1;
-// 	while (l<=r) {
-// 		int m = (l+r)/2;
-// 		if (v[m] == target) return m;
-// 		else if (v[m] > target) r=m-1;
-// 		else if (v[m] < target) l=m+1;
-// 	}
-// 	return l;
-// }
+int myCmp(const void *a, const void *b) {
+    lli sub = *(lli*)a - *(lli*)b;
+    if (sub > 0) return 1;
+    else if (sub < 0) return -1;
+    else return 0;
+}
 
+lli vecs[MAX_N][3];
 
 int main() {
 	// ios_base::sync_with_stdio(false);
 	// cin.tie(nullptr);
-	
 	int N;
 	lli a, b, c;
 	// cin >> N >> a >> b >> c;
 	scanf("%d %lld %lld %lld", &N, &a, &b, &c);
 	
-	vector< vector<lli> > vecs(N, vector<lli>(3));
 	vector<lli> sorted_int(N);
 	for (int i=0; i<N; i++) {
 		lli p_i, z_i;
@@ -76,34 +67,37 @@ int main() {
 	// remap x_i and y_i to a smaller range
 	sort(sorted_int.begin(), sorted_int.end());
 	auto uq = unique(sorted_int.begin(), sorted_int.end());
-	sorted_int.erase(uq, sorted_int.end());
+	// sorted_int.erase(unique(sorted_int.begin(), sorted_int.end()), sorted_int.end());
 
 	// sort by z_i
-	sort(vecs.begin(), vecs.end());
-	
+	qsort(vecs, N, 3*sizeof(lli), myCmp);
 	lli sum_d = 0;
 
 	// build a binary indexed tree to store x_i
-	vector<int> BIT(sorted_int.size()+10, 0);
-	vector<int> hold_x;
+	// vector<int> BIT(sorted_int.size()+10, 0);
+	vector<int> BIT(uq-sorted_int.begin()+10, 0);
+	int hold_x[N];
+	int holding=0;
 	lli old_z = MIN_Z - 1;
 	for (int i=0; i<N; i++) {
 		lli z_i = vecs[i][0];
-		int x_i = lower_bound(sorted_int.begin(), sorted_int.end(), vecs[i][1])-sorted_int.begin()+1;
-		int y_i = lower_bound(sorted_int.begin(), sorted_int.end(), vecs[i][2])-sorted_int.begin()+1;
+		// int x_i = lower_bound(sorted_int.begin(), sorted_int.end(), vecs[i][1])-sorted_int.begin()+1;
+		// int y_i = lower_bound(sorted_int.begin(), sorted_int.end(), vecs[i][2])-sorted_int.begin()+1;
+		int x_i = lower_bound(sorted_int.begin(), uq, vecs[i][1])-sorted_int.begin()+1;
+		int y_i = lower_bound(sorted_int.begin(), uq, vecs[i][2])-sorted_int.begin()+1;
 		if (z_i != old_z) {
 			// insert all holding x if on a new z value
-			while (! hold_x.empty()) {
-				insertBIT(BIT, hold_x.back());
-				hold_x.pop_back();
+			while (holding) {
+				holding--;
+				insertBIT(BIT, hold_x[holding]);
 			}
 			old_z = z_i;
 		}
 		// query prefix sum
-		lli query = (lli) queryBIT(BIT, y_i-1);
-		sum_d += query;
+		sum_d += (lli) queryBIT(BIT, y_i-1);
 		// add x_i to holding x
-		hold_x.push_back(x_i);
+		hold_x[holding] = x_i;
+		holding++;
 	}
 
 	// cout << sum_d << endl;
