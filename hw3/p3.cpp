3510e1d753c426a54cb58c74f0bb9a33d8b9d403
Frank Kuo <frankkuo@protonmail.com>
2021-12-07 22:25:58 +0800

    finish p4

---
+++ hw3/p3.cpp
@@ -0,0 +1,11 @@
+// Refs & ppl discussed with
+// b09902100
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
--- hw3/p4.cpp
+++ hw3/p4.cpp
@@ -1,3 +1,6 @@
+// Refs & ppl discussed with
+// b09902100
+
 #include <bits/stdc++.h>
 
 #define lli long long int
@@ -10,93 +13,58 @@ struct djs_node {
     djs_node(int _p, int _rk, int _sz) : p(_p), rk(_rk), sz(_sz) {}
 };
 
-int findSet(int val, map<int, djs_node> &djs) {
-    int p = djs[val].p;
-    if (p == val) {
+int findSet(int val, vector<djs_node> &djs) {
+    if (djs[val].p == val) {
         return val;
     } else {
-        int set_id = findSet(p, djs);
-        djs[val].p = set_id;
-        return set_id;
+        djs[val].p = findSet(djs[val].p, djs);
+        return djs[val].p;
     }
 }
 
-void unionSet(int sa, int sb, map<int, djs_node> &djs) {
-    // int sz_a=djs[sa].rk, sz_b=djs[sb].rk;
-    auto na=djs.find(sa), nb=djs.find(sb);
-    if (na->second.rk > nb->second.rk) {
-        nb->second.p = na->first;
-        na->second.sz += nb->second.sz;
-        nb->second.sz = 0;
-    } else if (na->second.rk < nb->second.rk) {
-        na->second.p = nb->first;
-        nb->second.sz += na->second.sz;
-        na->second.sz = 0;
-    } else {
-        nb->second.p = na->first;
-        na->second.rk += 1;
-        na->second.sz += nb->second.sz;
-        nb->second.sz = 0;
+lli unionSet(int sa, int sb, vector<djs_node> &djs) {
+    lli crowd_diff = (lli) djs[sa].sz * (lli) djs[sb].sz;
+    if (djs[sa].rk < djs[sb].rk) {
+        swap(sa, sb);
     }
+    if (djs[sa].rk == djs[sb].rk) {
+        djs[sa].rk += 1;
+    }
+    djs[sb].p = sa;
+    djs[sa].sz += djs[sb].sz;
+    return crowd_diff;
 }
 
-void placeFood(int p, int r, map<int, djs_node> &djs) {
-    if (!djs.count(p)) {
-        djs[p] = djs_node(p, 0, 0);
-    }
-    auto left = djs.lower_bound(p-r);
-    auto right = djs.upper_bound(p+r);
-    for (auto it = left; it!=right; it++) {
-        int mid_set = findSet(p, djs);
-        int merged_set = findSet(it->first, djs);
-        if (merged_set == it->first && merged_set != mid_set) {
-            unionSet(mid_set, merged_set, djs);
+void placeFood(int p, int r, vector<djs_node> &djs, map<int, int> &garden, lli &crowd) {
+    auto left=garden.lower_bound(p-r);
+    auto right=garden.upper_bound(p+r);
+    for (auto it=left; it!=right; it++) {
+        int main_set = findSet(left->second, djs);
+        int merged_set = findSet(it->second, djs);
+        if (main_set != merged_set) {
+            crowd += unionSet(main_set, merged_set, djs);
         }
     }
-    int tmp = findSet(p, djs);
-    if (tmp != p) {
-        djs[p].sz = djs[tmp].sz;
-        djs[p].rk = djs[tmp].rk;
-        djs[tmp].sz = 0;
-        djs[tmp].p = p;
-        djs[p].p = p;
+    if (left != right) {
+        int boss = findSet(left->second, djs);
+        garden.erase(left, right);
+        garden[p] = boss;
     }
 }
 
-void moveCat(int t, int x, map<int, djs_node> &djs, vector<int> &cat_id) {
-    int og_set = findSet(cat_id[t], djs);
+void moveCat(int t, int x, vector<djs_node> &djs, vector<int> &djs_idx, map<int, int> &garden, lli &crowd) {
+    int og_set = findSet(djs_idx[t], djs);
     djs[og_set].sz -= 1;
-    cat_id[t] = x;
-    if (!djs.count(x)) {
-        djs[x] = djs_node(x, 0, 1);
+    crowd -= (lli) djs[og_set].sz;
+    djs_idx[t] = djs.size();
+    djs.push_back(djs_node(djs_idx[t], 0, 1));
+    if (!garden.count(x)) {
+        garden[x] = djs_idx[t];
     } else {
-        if (findSet(x, djs) == x) {
-            if (djs[x].rk == 0) djs[x].rk+=1;
-            djs[x].sz += 1;
-        } else {
-            djs[x] = djs_node(x, 0, 1);
-        }
+        int main_set = findSet(garden[x], djs);
+        crowd += unionSet(main_set, djs_idx[t], djs);
+        garden[x] = findSet(djs_idx[t], djs);
     }
-}
-
-lli calcCrowd(map<int, djs_node> &djs) {
-    lli ans=0;
-    for (auto it=djs.begin(); it!=djs.end(); it++) {
-        if (it->second.sz > 0) {
-            ans += (lli)(it->second.sz) * ((lli)(it->second.sz)-1) / 2;
-        }
-    }
-    return ans;
-}
-
-
-void printMap(map<int, djs_node> m) {
-    cout << "===== printing map =====\n";
-    for (auto it=m.begin(); it!=m.end(); it++) {
-        cout << "key: " << it->first << "\n";
-        cout << "p: " << it->second.p << ", rk: " << it->second.rk << ", sz: " << it->second.sz << "\n";
-    }
-    cout << "======= finished =======\n";
 }
 
 int main() {
@@ -106,41 +74,31 @@ int main() {
     int n, q;
     cin >> n >> q;
     
-    // map<int, int> djs_p, djs_rk;
-    map<int, djs_node> djs;
-    vector<int> cat_id(n+1);
+    vector<djs_node> djs(n+1);
+    vector<int> djs_idx(n+1);
+    map<int, int> garden;
+    lli crowd=0;
     for (int i=1; i<=n; i++) {
         int pos_x;
         cin >> pos_x;
-        cat_id[i] = pos_x;
-        if (!djs.count(pos_x)) {
-            djs[pos_x] = djs_node(pos_x, 0, 1);
+        djs_idx[i] = i;
+        djs[i] = djs_node(i, 0, 1);
+        if (!garden.count(pos_x)) {
+            garden[pos_x] = i;
         } else {
-            djs[pos_x].rk = 1;
-            djs[pos_x].sz += 1;
+            crowd += unionSet(garden[pos_x], i, djs);
         }
     }
-
-    // printMap(djs_p);
-    // printMap(djs_rk);
 
     for (int i=0; i<q; i++) {
         int op, arg1, arg2;
         cin >> op >> arg1 >> arg2;
-
         if (op == 1) {
-            placeFood(arg1, arg2, djs);
+            placeFood(arg1, arg2, djs, garden, crowd);
         } else {
-            moveCat(arg1, arg2, djs, cat_id);
+            moveCat(arg1, arg2, djs, djs_idx, garden, crowd);
         }
-
-        // for (int j=1; j<=n; j++) {
-        //     cat_id[j] = findSet(cat_id[j], djs_p);
-        // }
-        // printMap(djs_p);
-        // printMap(djs_rk);
-
-        cout << calcCrowd(djs) << "\n";
+        cout << crowd << "\n";
     }
 
     return 0;
